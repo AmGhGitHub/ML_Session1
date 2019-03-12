a594fdcd42baa05012d82679f3c56145071f7283
Am Gh <altghaderi@gmail.com>
2019-03-12 14:18:19 -0600

    added gradient descent
    Signed-off-by: Am Gh <altghaderi@gmail.com>

--- multivariate_linearregression.py
+++ multivariate_linearregression.py
@@ -3,33 +3,9 @@ import numpy as np
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
+from sklearn.datasets import load_boston
 
-df_capita = pd.read_csv('data/canada_per_capita_income.csv', header=[0])
-X = df_capita.iloc[:, 0].values
-y = df_capita.iloc[:, 1].values
-X = X[:, np.newaxis]
-y = y[:, np.newaxis]
-poly_dg = 2
-scaler = PolynomialFeatures(degree=poly_dg)
-X2 = scaler.fit_transform(X)
-
-slr = LinearRegression()
-plr = LinearRegression()
-slr.fit(X, y)
-plr.fit(X2, y)
-
-y_prd_slr = slr.predict(X)
-y_prd_plr = plr.predict(X2)
-
-print(slr.score(X, y))
-print(plr.score(X2, y))
-
-fig, ax = plt.subplots()
-
-ax.scatter(X, y, label='Data Points', color='black', s=30, marker='o')
-ax.plot(X, y_prd_slr, color='blue', linewidth=4, label='Fitted line')
-ax.plot(X, y_prd_plr, color='green', linewidth=4, label=f'Polynomial degree {poly_dg}')
-ax.vlines(X, y, slr.predict(X), colors='red', linestyles='--', label='residuals')
-ax.grid(True)
-ax.legend(loc=0)
-plt.show()
+# https://bigdata-madesimple.com/how-to-run-linear-regression-in-python-scikit-learn/
+boston_house = load_boston()
+df_bh = pd.DataFrame(data=boston_house.data, columns=boston_house.feature_names)
+df_bh['price'] = boston_house.target
--- simple_linear_regression.py
+++ simple_linear_regression.py
@@ -9,7 +9,7 @@ X = df_capita.iloc[:, 0].values
 y = df_capita.iloc[:, 1].values
 X = X[:, np.newaxis]
 y = y[:, np.newaxis]
-poly_dg = 2
+poly_dg = 3
 scaler = PolynomialFeatures(degree=poly_dg)
 X2 = scaler.fit_transform(X)
 
@@ -27,8 +27,8 @@ print(plr.score(X2, y))
 fig, ax = plt.subplots()
 
 ax.scatter(X, y, label='Data Points', color='black', s=30, marker='o')
-ax.plot(X, y_prd_slr, color='blue', linewidth=4, label='Fitted line')
-ax.plot(X, y_prd_plr, color='green', linewidth=4, label=f'Polynomial degree {poly_dg}')
+ax.plot(X, y_prd_slr, color='blue', linewidth=3, label='Fitted line')
+ax.plot(X, y_prd_plr, color='green', linewidth=3, label=f'Polynomial degree {poly_dg}')
 #ax.vlines(X, y, slr.predict(X), colors='red', linestyles='--', label='residuals')
 ax.grid(True)
 ax.legend(loc=0)
