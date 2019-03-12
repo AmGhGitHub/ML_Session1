9f0477c9aaf435335dcad397bfeb36dee1211db3
Am Gh <altghaderi@gmail.com>
2019-03-12 10:00:33 -0600

    added data files
    Signed-off-by: Am Gh <altghaderi@gmail.com>

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
 
-b=2
+df_capita = pd.read_csv('data/canada_per_capita_income.csv', header=[0])
+X = df_capita.iloc[:, 0].values
+y = df_capita.iloc[:, 1].values
+X = X[:, np.newaxis]
+y = y[:, np.newaxis]
+poly_dg = 1
+scaler = PolynomialFeatures(degree=poly_dg)
+X2 = scaler.fit_transform(X)
 
-x = np.arange(10)
-y = np.sin(x)
-plt.plot(x, y)
+slr = LinearRegression()
+plr = LinearRegression()
+slr.fit(X, y)
+plr.fit(X2, y)
+
+fig, ax = plt.subplots()
+
+ax.scatter(X, y, label='Data Points', color='black', s=30, marker='o')
+ax.plot(X, slr.predict(X), color='blue', linewidth=4, label='Fitted line')
+ax.plot(X, plr.predict(X2), color='green', linewidth=4, label=f'Polynomial degree {poly_dg}')
+ax.vlines(X, y, slr.predict(X), colors='red', linestyles='--', label='residuals')
+ax.grid(True)
+ax.legend(loc=0)
 plt.show()
