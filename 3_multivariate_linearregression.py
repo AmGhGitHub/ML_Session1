2355b1e2e393e68aa5e77b2e72bf69e6ab9c6986
Am Gh <altghaderi@gmail.com>
2019-03-15 10:04:37 -0600

    added gradient descent
    Signed-off-by: Am Gh <altghaderi@gmail.com>

--- 3_multivariate_linearregression.py
+++ 3_multivariate_linearregression.py
@@ -7,5 +7,13 @@ from sklearn.datasets import load_boston
 
 # https://bigdata-madesimple.com/how-to-run-linear-regression-in-python-scikit-learn/
 boston_house = load_boston()
-df_bh = pd.DataFrame(data=boston_house.data, columns=boston_house.feature_names)
-df_bh['price'] = boston_house.target
+
+# boston data is a dictionary, so we can get an idea about the data structure using keys
+
+# print(boston_house.keys())
+print(boston_house['DESCR'])
+
+features = [feat.lower() for feat in boston_house['feature_names']]
+
+df_boston = pd.DataFrame(data=boston_house['data'], columns=features)
+df_boston['price'] = boston_house['target']
